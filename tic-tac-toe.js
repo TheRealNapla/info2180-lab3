12e9ce656ca1103926715b4e4f8c372b9b7ab4a9
Peter Thelwell <pthelwell55@gmail.com>
2022-10-11 18:06:41 -0500

    Added basic game functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,38 @@ function boardLayout(){
     }
 }
 
-document.addEventListener("DOMContentLoaded",function(){
+function play(){
+    var turn = 1;
+    var layout = document.getElementById("board").getElementsByTagName("div");
+    for (const space of layout){
+        space.addEventListener("mouseover", function(){
+            if (space.textContent == ""){
+                space.classList.add("hover");
+            }
+        })
+        space.addEventListener("mouseout", function(){
+            space.classList.remove("hover");
+        })
+        space.addEventListener("click", function(){
+            if (turn == 1){
+                if (space.textContent != "O"){
+                    space.classList.add("X");
+                    space.textContent = "X";
+                    turn = 0;
+                }
+            }
+            else if (turn == 0){
+                if (space.textContent != "X"){
+                    space.classList.add("O");
+                    space.textContent = "O";
+                    turn = 1;
+                }
+            }
+        })
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
     boardLayout();
+    play();
 })
